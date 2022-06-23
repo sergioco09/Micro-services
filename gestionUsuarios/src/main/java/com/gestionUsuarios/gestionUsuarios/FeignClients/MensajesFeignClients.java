baadb937060dfd5709457998fa44b25bb8ec8ec8
sergioco09 <SergioChaconO93@gmail.com>
2022-06-23 14:59:49 -0500

    Nueva version

--- gestionUsuarios/src/main/java/com/gestionUsuarios/gestionUsuarios/FeignClients/MensajesFeignClients.java
+++ gestionUsuarios/src/main/java/com/gestionUsuarios/gestionUsuarios/FeignClients/MensajesFeignClients.java
@@ -3,7 +3,7 @@ package com.gestionUsuarios.gestionUsuarios.FeignClients;
 import com.gestionUsuarios.gestionUsuarios.models.ConsultaMensajesDTO;
 import com.gestionUsuarios.gestionUsuarios.models.CrearMensajeDTO;
 import com.gestionUsuarios.gestionUsuarios.models.ResponseMensajeDTO;
-import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "mensajes-service", url="http://localhost:8083", path = "gestion/mensajes/v1")
+//@FeignClient(name = "mensajes-service", url="http://localhost:8083", path = "gestion/mensajes/v1")
 public interface MensajesFeignClients {
 
     @PostMapping(value = "/crearMensaje")
--- gestionUsuarios/src/main/java/com/gestionUsuarios/gestionUsuarios/FeignClients/PublicacionesFeignClients.java
+++ gestionUsuarios/src/main/java/com/gestionUsuarios/gestionUsuarios/FeignClients/PublicacionesFeignClients.java
@@ -3,7 +3,7 @@ package com.gestionUsuarios.gestionUsuarios.FeignClients;
 import com.gestionUsuarios.gestionUsuarios.models.CrearPublicacionDTO;
 import com.gestionUsuarios.gestionUsuarios.models.ResponseActualizaPubDTO;
 import com.gestionUsuarios.gestionUsuarios.models.ResponseAltaPubliDTO;
-import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "publicaciones-service", url = "http://localhost:8084", path = "gestion/publicaciones/v1")
+//@FeignClient(name = "publicaciones-service", url = "http://localhost:8084", path = "gestion/publicaciones/v1")
 public interface PublicacionesFeignClients {
 
     @PostMapping(value = "/crearPublicacion")
